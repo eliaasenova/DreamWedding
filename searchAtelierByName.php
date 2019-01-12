b1f1fb8ec0ce283625b52144ada9c42c42a5114d
egotewa <egotewa@gmail.com>
2019-01-12 23:57:35 +0200

    Changed inner queries and db connections to remote curl invocations for establishing communications out of the current system(localhost)

--- searchAtelierByName.php
+++ searchAtelierByName.php
@@ -1,19 +1,16 @@
 <?php 
+	/**
+	 * This php file is responsible for mapping the client queries to remote REST API Call and returning
+	 * the response to the client
+	 */
 	$myName=$_GET["atelName"];
-	$conn=new PDO('mysql:host=localhost;dbname=dreamweddingdatabase;charset=utf8', 'root', '');
-	$statement=$conn->query("SELECT * FROM atelier WHERE name='$myName'");
-	while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-    $result[] = $row;
-	}
-	/*if(count($result)==0)
-	{
-		echo '<script> document.getElementById("notFound").style.display="inline"; </script>';
-	}
-	else
-	{*/
-		
-		echo json_encode($result);
-	
-	//}
+	$ateliersServerSocket = "192.168.1.101:8080";
+	$service_url = 'http://' . $ateliersServerSocket . '/DreamWeddingAteliersServer/ateliers/' . $myName;
+	$curl = curl_init($service_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$curl_response = curl_exec($curl);
+
+	echo $curl_response;
+	curl_close($curl);
 	
 ?>
