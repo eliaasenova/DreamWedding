746a3b795f3e43d6a05b69dae27429f858ee5d0b
egotewa <egotewa@gmail.com>
2019-01-12 23:55:22 +0200

    Removed requirements for ateliers table from config.php as they are no longer needed upon setup

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
 	require 'create_db.php';
 	require 'create_users_table.php';
-	require 'create_atelier_table.php';
 	require 'create_guestcategories_table.php';
-	require 'fill_atelier_table.php';
 	require 'fill_guestcategories_table.php';
 	require 'create_tables.php';
 	require 'fill_hairdressingsalons_table.php';
